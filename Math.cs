745db20d88f0504d11cd100eed39b38d9b0a6efb
VerSS <verss560@gmail.com>
2019-02-25 22:18:30 +0200

    second commit

--- Math.cs
+++ Math.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Test
 {
-    class Math
+    public static class Math
     {
+        public static void BinPow(int a, int n)
+        {
+            var use = 0;
+            if (use == n)
+            {
+                Console.WriteLine($"n={use},a={a}");
+            }
+            else
+            {
+                a *= a;
+                BinPow(a,n);
+                use++;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,33 +11,38 @@ namespace Test
     class Program
     {
         static void Main(string[] args)
-        { 
-            int age;
-            string name;
-            string bio;
-            List<User> users =  new List<User>();
-            var user = new User();
-            int n=Convert.ToInt32(Console.ReadLine());
+        {   
+            int a = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            for (int i=0; i<n;i++)
-            { 
-                age = Convert.ToInt32(Console.ReadLine());
-                name = Console.ReadLine();
-                bio = Console.ReadLine();
-                if (bio == ".end")
-                {
-                    bio = null;
-                    users.Add(new User(name, bio, age));
-                    break;
-                }
-                users.Add(new User(name, bio, age));
-            }
-            users.OrderBy(e => e.Age);
-            foreach(var u in users)
-            {
-                u.GetInfo();
-            }
-            
+            Math.BinPow(a,n);
+
+            //int age;
+            //string name;
+            //string bio;
+            //List<User> users =  new List<User>();
+            //var user = new User();
+            //int n=Convert.ToInt32(Console.ReadLine());
+
+            //for (int i=0; i<n;i++)
+            //{ 
+            //    age = Convert.ToInt32(Console.ReadLine());
+            //    name = Console.ReadLine();
+            //    bio = Console.ReadLine();
+            //    if (bio == ".end")
+            //    {
+            //        bio = null;
+            //        users.Add(new User(name, bio, age));
+            //        break;
+            //    }
+            //    users.Add(new User(name, bio, age));
+            //}
+            //users.OrderBy(e => e.Age);
+            //foreach(var u in users)
+            //{
+            //    u.GetInfo();
+            //}
+
             Console.ReadKey();
 
         }
