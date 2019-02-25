f6a5f6d4c4bcddfb0ab431c8952ee2b4dde29689
VerSS <verss560@gmail.com>
2019-02-25 16:46:49 +0300

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int age;
+            string name;
+            string bio;
+            List<User> users =  new List<User>();
+            var user = new User();
+            int n=Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i<n;i++)
+            { 
+                age = Convert.ToInt32(Console.ReadLine());
+                name = Console.ReadLine();
+                bio = Console.ReadLine();
+                if (bio == ".end")
+                {
+                    bio = null;
+                    users.Add(new User(name, bio, age));
+                    break;
+                }
+                users.Add(new User(name, bio, age));
+            }
+            users.OrderBy(e => e.Age);
+            foreach(var u in users)
+            {
+                u.GetInfo();
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class User
+    {
+        public int Age;
+        public string Name;
+        public string Bio;
+        public User()
+        {
+            
+        }
+        public User(string name, string bio, int age)
+        {
+            Name = name;
+            Bio = bio;
+            Age = age;
+        }
+
+        public User(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public User(int age)
+        {
+            Age = age;
+        }
+
+        public void GetInfo()
+        {   
+            Console.WriteLine($"{Name} {Age} {Bio}");
+        }
+
+    }
+}
